c1780a2c925f75525c4a065af2f2f0734d8a51dd
vishaltanawade688@gmail.com <vishaltanawade688@gmail.com>
2022-05-25 17:04:12 +0530

    Empty Controller added

---
+++ ASPDotNetWebApplicationDotNetFramework/Controllers/CustomerPortalController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ASPDotNetWebApplicationDotNetFramework.Controllers
+{
+    public class CustomerPortalController : Controller
+    {
+        // GET: CustomerPortal
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
